66ecede7ee73829370dff7ed238f834c15d7bbef
Juan Guevara <soyjuandavidguevara@gmail.com>
2025-08-17 20:05:02 -0500

    Migrated qr_code scanner to use load balancer

--- src/services/qrcode_detection/QrCodeScanner.py
+++ src/services/qrcode_detection/QrCodeScanner.py
@@ -2,38 +2,39 @@ import rospy
 import time
 
 from perception_msgs.srv import read_qr_srv, read_qr_srvRequest
-from sensor_msgs.msg import Image
-from robot_toolkit_msgs.msg import vision_tools_msg
-from robot_toolkit_msgs.srv import vision_tools_srv
-from config import VisionModuleConfiguration
 import constants
 
 import cv2
 from cv_bridge import CvBridge
+from utils.camera_topic import CameraTopic
+from cv2.types import MatLike
 
 
 class QrCodeScanner:
     bridge = CvBridge()
-    image = None
+    detected = None
 
     def __init__(self, camera: str):
         rospy.Service(constants.SERVICE_READ_QR, read_qr_srv, self.callback)
-        rospy.Subscriber(camera, Image, self.camera_subscriber)
+        self.camera = CameraTopic(camera)
+        self.detected = None
 
-    def camera_subscriber(self, msg: Image):
-        self.image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+    def camera_subscriber(self, image: MatLike):
+        detector = cv2.QRCodeDetector()
+        try:
+            value, _, _ = detector.detectAndDecode(image)
+            if value != "":
+                self.detected = value
+        except:
+            pass
 
     def callback(self, request: read_qr_srvRequest):
+        sid = self.camera.subscribe(self.camera_subscriber, wait_turns=1)
         start = time.time()
 
         while time.time() - start < request.timeout:
-            try:
-                detector = cv2.QRCodeDetector()
-
-                value, _, _ = detector.detectAndDecode(self.image)
-
-                if value != "":
-                    return value
-            except:
-                continue
-        return ""
+            if self.detected is not None:
+                break
+            time.sleep(0.1)
+        self.camera.unsubscribe(sid)
+        return self.detected if self.detected is not None else ""
